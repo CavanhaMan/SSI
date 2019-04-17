3eb5358e40526994d80629fbe617ab9a5a9fcb36
CavanhaMan <cavanhaman@hotmail.com>
2019-04-16 21:48:52 -0300

    Add files via upload

---
+++ Trabalho_Miller-Rabin/Miller-Rabin.cpp
@@ -0,0 +1,25 @@
+/*-------------------*
+* Rodrigo CavanhaMan *
+*     IFTM - SSI     *
+*--------------------*/
+#include<stdio.h>
+#include <stdlib.h> 
+#include<iostream>
+using namespace std;
+
+int main(void)	{
+//TEST (n)
+	int n, k, q;
+	//1. Encontre inteiros k, q, com k > 0, q ímpar, de modo que (n – 1 = 2kq);
+	n-1=2^k*q;
+//2. Selecione um inteiro aleatório a, 1 < a < n – 1;
+//3. if aq mod n = 1 then return(“inconclusive”);
+//4. for j = 0 to k – 1 do
+//5. if a2jq mod n = n - 1 then return(“inconclusive”);
+//6. return(“composite”);
+
+//terminado em 0 ou 5 nao é primo
+//divisivel por 2 não é primo
+
+}
+
